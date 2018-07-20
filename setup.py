d56284481b2279bc799260cfcc6713630973f22b
Nathan <nathan@vertile.com>
2018-07-20 15:59:15 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile['packages'], r=False)
+
+setuptools.setup(
+    name='Test',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    entry_points={'console_scripts':['test=test:main']},
+    include_package_data=True
+)
